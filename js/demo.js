1ce658d3cc182cafc1395c8f051e13d8cbd5618a
Mhc-inc <Wf6350177@163.com>
2024-07-16 11:12:36 +0800

    Site updated: 2024-07-16 11:12:36

---
+++ js/demo.js
@@ -0,0 +1,20 @@
+if(typeof danMu === 'function') {
+    danMu(
+          () => [
+              {avatar:"/img/mhc.png",text:'举杯邀明月，对影成三人',url:"https://mhcincapi.top"},
+              {avatar:"/img/mhc.png",text:'会当凌绝顶，一览众山小',url:"https://mhcincapi.top"},
+              {avatar:"/img/mhc.png",text:'云想衣裳花想容，春风拂槛露华浓',url:"https://mhcincapi.top"},
+              {avatar:"/img/mhc.png",text:'二十四桥明月夜，玉人何处教吹箫？',url:"https://mhcincapi.top"},
+              {avatar:"/img/mhc.png",text:'月落乌啼霜满天，江枫渔火对愁眠。',url:"https://mhcincapi.top"},
+              {avatar:"/img/mhc.png",text:'两岸猿声啼不住，轻舟已过万重山。',url:"https://mhcincapi.top"},
+              {avatar:"/img/mhc.png",text:'泠泠七弦上，静听松风寒。',url:"https://mhcincapi.top"},
+              {avatar:"/img/mhc.png",text:'举杯邀明月，对影成三人',url:"https://mhcincapi.top"},
+              {avatar:"/img/mhc.png",text:'会当凌绝顶，一览众山小',url:"https://mhcincapi.top"},
+              {avatar:"/img/mhc.png",text:'云想衣裳花想容，春风拂槛露华浓',url:"https://mhcincapi.top"},
+              {avatar:"/img/mhc.png",text:'二十四桥明月夜，玉人何处教吹箫？',url:"https://mhcincapi.top"},
+              {avatar:"/img/mhc.png",text:'月落乌啼霜满天，江枫渔火对愁眠。',url:"https://mhcincapi.top"},
+              {avatar:"/img/mhc.png",text:'两岸猿声啼不住，轻舟已过万重山。',url:"https://mhcincapi.top"},
+              {avatar:"/img/mhc.png",text:'泠泠七弦上，静听松风寒。',url:"https://mhcincapi.top"}
+          ]
+          );
+}
